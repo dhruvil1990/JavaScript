de8b147d115874b3ecd6bc9e9f62b7d8231d491f
Dhruvil <pateldhruvil1990@gmail.com>
2016-10-31 19:36:51 -0700

    Add files via upload

--- Quick Sort.js
+++ Quick Sort.js
@@ -1,43 +1,41 @@
+// Sorting by done by partition swaping left and right from the pivot point.
 function quickSort(arr, left, right){
    var len = arr.length, 
    pivot,
-   partitionIndex;
+   partIndex;
 
 
   if(left < right){
     pivot = right;
-    partitionIndex = partition(arr, pivot, left, right);
+    partIndex = partition(arr, pivot, left, right);
     
    //sort left and right
-   quickSort(arr, left, partitionIndex - 1);
-   quickSort(arr, partitionIndex + 1, right);
+   quickSort(arr, left, partIndex - 1);
+   quickSort(arr, partIndex + 1, right);
   }
   return arr;
-}
-        
+}        
 
-
+// Partition function smaller value to pivot piont is moved left and larger values to right
 function partition(arr, pivot, left, right){
-   var pivotValue = arr[pivot],
-       partitionIndex = left;
+   var pivotVal = arr[pivot],
+       partIndex = left;
 
    for(var i = left; i < right; i++){
-    if(arr[i] < pivotValue){
-      swap(arr, i, partitionIndex);
-      partitionIndex++;
+    if(arr[i] < pivotVal){
+      swap(arr, i, partIndex);
+      partIndex++;
     }
   }
-  swap(arr, right, partitionIndex);
-  return partitionIndex;
+  swap(arr, right, partIndex);
+  return partIndex;
 }
         
-
-
+//Swap values
 function swap(arr, i, j){
    var temp = arr[i];
    arr[i] = arr[j];
    arr[j] = temp;
 }
-
-quickSort([16,11,9,7,6,5,3, 2],0,7);
-//[2, 3, 5, 6, 7, 9, 11, 16]
+quickSort([10,8,16,3,2,7,4],0,7) 
+//[2, 3, 4, 7, 8, 10, 16]
